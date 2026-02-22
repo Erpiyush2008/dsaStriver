7358aa52e67fcd5d818db75d5186328fd52cb377
Erpiyush2008 <piyushsharma190708@gmail.com>
2026-02-22 12:33:53 +0530

    hello

---
+++ SudokuSolver.java
@@ -0,0 +1,89 @@
+public class SudokuSolver {
+
+    public static boolean isSafe(int[][] board, int row, int col, int num) {
+
+        // Check row
+        for (int d = 0; d < 9; d++) {
+            if (board[row][d] == num) {
+                return false;
+            }
+        }
+
+        // Check column
+        for (int r = 0; r < 9; r++) {
+            if (board[r][col] == num) {
+                return false;
+            }
+        }
+
+        // Check 3x3 grid
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+
+        for (int r = startRow; r < startRow + 3; r++) {
+            for (int d = startCol; d < startCol + 3; d++) {
+                if (board[r][d] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean solveSudoku(int[][] board) {
+
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+
+                if (board[row][col] == 0) {
+
+                    for (int num = 1; num <= 9; num++) {
+
+                        if (isSafe(board, row, col, num)) {
+                            board[row][col] = num;
+
+                            if (solveSudoku(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = 0; // Backtracking
+                            }
+                        }
+                    }
+                    return false; // No number fits
+                }
+            }
+        }
+        return true; // Solved
+    }
+
+    public static void printBoard(int[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] board = {
+            {3,0,6,5,0,8,4,0,0},
+            {5,2,0,0,0,0,0,0,0},
+            {0,8,7,0,0,0,0,3,1},
+            {0,0,3,0,1,0,0,8,0},
+            {9,0,0,8,6,3,0,0,5},
+            {0,5,0,0,9,0,6,0,0},
+            {1,3,0,0,0,0,2,5,0},
+            {0,0,0,0,0,0,0,7,4},
+            {0,0,5,2,0,6,3,0,0}
+        };
+
+        if (solveSudoku(board)) {
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists");
+        }
+    }
+}
