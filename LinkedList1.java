3e74ebfaa5c32cffa6aa2e73d956bece77291d17
Erpiyush2008 <piyushsharma190708@gmail.com>
2026-02-28 12:21:20 +0530

    hello

---
+++ LinkedList1.java
@@ -0,0 +1,60 @@
+public class LinkedList1 {
+    public static class Node {
+    int data;
+    Node next;
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+        
+    }
+}
+
+public  static Node head;
+public static Node tail;
+
+public void print() {
+    if(head == null) {
+        System.out.println("Linked List is empty");
+        return;
+    }
+
+    Node temp = head;
+    while (temp != null) {
+        System.out.print(temp.data + " ->");
+        temp = temp.next;
+    }
+    System.out.println("null");
+}
+
+public void addFirst(int data) {
+    Node newNode = new Node(data);
+    if (head == null) {
+        head = tail = newNode;
+        return;
+    }
+    newNode.next = head;
+    head = newNode;
+}
+public void addLast(int data) {
+    Node newNode = new Node(data);
+    if (head == null) {
+        head = tail = newNode;
+        return;
+    }
+    tail.next = newNode;
+    tail = newNode;
+}
+
+
+ 
+public static void main(String[] args) {
+    LinkedList1 l1 = new LinkedList1();
+   l1.addFirst(2);
+   l1.addFirst(1);
+   l1.addLast(3);
+   l1.addLast(4);
+   l1.print();
+
+
+}
+}
